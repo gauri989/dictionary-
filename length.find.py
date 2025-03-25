5248dc3013fc8d3ea6ec3fc594c606d039cea656
gauri989 <gauripareek28@gmail.com>
2025-03-25 13:57:11 +0530

    Add files via upload

---
+++ length.find.py
@@ -0,0 +1,6 @@
+# 2. you are given a list of subjects for students.Assume one classroom is required for 1 subject.how many classrooms are needed by all students.
+# "python" ,"java" ,"c++" ,"python" ,"javascript","java"
+# "python" , "java" , "c++" , "c"  
+set="python" ,"java" ,"c++" ,"python" ,"javascript","java","python" , "java" , "c++" , "c"  
+print(len.set)
+
